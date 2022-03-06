63327f680960318ed58d1aafe5a3ad37c0e80f71
R-Link Developer <r-linkdeveloper@MacBook-Air-de-R-Link.local>
2022-03-06 17:42:09 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component, useCallback } from 'react';
+import { 
+    View, 
+    Text
+} from 'react-native';
+import Home from '../screens/Home'
+
+export default App = () => {
+
+    return(
+        <Home />
+    )
+}
